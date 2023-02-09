6955eae38653214e9697ad8c57dd9e5c6a6fcccf
maxmeng93 <mengyucheng@yogorobot.com>
2023-02-09 17:45:27 +0800

    update

--- 1.shaping_functions/1.glsl
+++ 1.shaping_functions/1.glsl
@@ -7,13 +7,13 @@ uniform vec2 u_mouse;
 uniform float u_time;
 
 float plot(vec2 st){
-  // return smoothstep(0.02, 0.0, abs(st.y - st.x));
-  return 1.0 - pow(abs(st.x), 0.5);
+  return smoothstep(0.02, 0.0, abs(st.y - st.x));
 }
 
-void main() {
+void main(){
+  // 归一化（0.0 - 1.0）
   vec2 st = gl_FragCoord.xy / u_resolution;
-
+  
   // 0-1
   float y = st.x;
   
@@ -24,3 +24,10 @@ void main() {
   
   gl_FragColor = vec4(color, 1.0);
 }
+
+/**
+ * 一、左上角 (0.0, 1.0)
+ * st = vec2(0.0, 1.0);
+ * y = st.x = 0.0;
+ * color = vec3(0.0); 黑色
+ */
