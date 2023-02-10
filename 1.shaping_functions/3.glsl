2c9b680ac00282559e416f11b9c8882d0e3ce236
maxmeng93 <mengyucheng@yogorobot.com>
2023-02-10 10:33:10 +0800

    update

--- 1.shaping_functions/3.glsl
+++ 1.shaping_functions/3.glsl
@@ -2,32 +2,26 @@
 precision mediump float;
 #endif
 
+// 定义一个宏，每当程序调用PI时就用这个数来替换它
+#define PI 3.14159265359
+
 uniform vec2 u_resolution;
 uniform vec2 u_mouse;
 uniform float u_time;
 
-float plot(vec2 st) {
-  return smoothstep(0.02, 0.0, abs(st.y - st.x));
+float plot(vec2 st, float pct) {
+  return smoothstep(pct - 0.02, pct, st.y) - smoothstep(pct, pct + 0.02, st.y);
 }
 
 void main() {
-  // 归一化（0.0 - 1.0）
   vec2 st = gl_FragCoord.xy / u_resolution;
   
-  // 0-1
-  float y = st.x;
+  float y = pow(st.x, 5.0);
   
   vec3 color = vec3(y);
   
-  float pct = plot(st);
+  float pct = plot(st, y);
   color = (1.0 - pct) * color + pct * vec3(0.0, 1.0, 0.0);
   
   gl_FragColor = vec4(color, 1.0);
 }
-
-/**
-* 一、左上角 (0.0, 1.0)
-* st = vec2(0.0, 1.0);
-* y = st.x = 0.0;
-* color = vec3(0.0); 黑色
-*/
